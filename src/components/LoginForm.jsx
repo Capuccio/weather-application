b6953d6a57b6c71f774bfa577a49226c127e2680
Capuccio <jmcordova1999@gmail.com>
2020-10-16 18:08:40 -0400

    Normal login finished

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,11 +1,57 @@
 import React from "react";
-import { Button, DatePicker, message, Alert } from "antd";
+import { Form, Input, Button, Space, Checkbox, message, Alert } from "antd";
+import {
+	UserOutlined,
+	LockOutlined,
+	EyeInvisibleOutlined,
+	EyeTwoTone
+} from "@ant-design/icons";
 
-const LoginForm = props => {
+import "./css/LoginForm.css";
+
+const LoginForm = ({ rememberMe }) => {
 	return (
-		<div>
-			<p>Prueba</p>
-		</div>
+		<Form className="login_form" name="login_form">
+			<Form.Item
+				name="user"
+				rules={[{ required: true, message: "Debe colocar su usuario" }]}
+			>
+				<Input
+					placeholder="Username or email"
+					prefix={<UserOutlined />}
+					style={{ borderRadius: "5px", width: "60%", padding: "3%" }}
+				/>
+			</Form.Item>
+			<Form.Item
+				name="pass"
+				rules={[{ required: true, message: "Debe colocar su clave" }]}
+			>
+				<Input.Password
+					placeholder="Password"
+					prefix={<LockOutlined />}
+					iconRender={(visible) =>
+						visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+					}
+					style={{ borderRadius: "5px", width: "60%", padding: "3%" }}
+				/>
+			</Form.Item>
+
+			<Space size={110}>
+				<Checkbox onChange={rememberMe}>Remember me</Checkbox>
+				<Button type="primary" size="large">
+					LOGIN
+				</Button>
+			</Space>
+
+			<Space size={108}>
+				<Button type="link" size="small">
+					Register now
+				</Button>
+				<Button type="link" size="small">
+					Forgot password?
+				</Button>
+			</Space>
+		</Form>
 	);
 };
 
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -5,12 +5,17 @@ import "./css/App.css";
 import LoginForm from "../components/LoginForm";
 
 function App() {
+	const rememberMe = () => console.log("Changed");
+
 	return (
 		<div className="card">
 			<div className="card__column">
-				<LoginForm />
+				<LoginForm rememberMe={rememberMe} />
+				<div className="card__separator">
+					or
+				</div>
 			</div>
-			<div className="card__column">Dos</div>
+			<div className="card__column">Imagen</div>
 		</div>
 	);
 }

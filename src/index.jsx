8ad2005edbcab4882f2587e4cce6fa054b40c31c
Capuccio <jmcordova1999@gmail.com>
2020-10-17 12:36:06 -0400

    changed from ^ to ~ in package

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import App from './pages/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import App from "./pages/App";
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));

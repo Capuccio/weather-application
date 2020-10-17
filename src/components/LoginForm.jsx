be5d8d0bce26a5ac105964f0e4b8dd3c2451843b
Capuccio <jmcordova1999@gmail.com>
2020-10-16 20:53:19 -0400

    Mobile version finished

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Form, Input, Button, Space, Checkbox, message, Alert } from "antd";
+import React, { useState, useEffect } from "react";
+import { Form, Input, Button, Space, Checkbox } from "antd";
 import {
 	UserOutlined,
 	LockOutlined,
@@ -9,7 +9,34 @@ import {
 
 import "./css/LoginForm.css";
 
-const LoginForm = ({ rememberMe }) => {
+const LoginForm = ({ rememberMe, handleLoginForm, formData }) => {
+	const [screenSize, setScreenSize] = useState(null);
+	const [optionsBelow, setOptionsBelow] = useState({
+		size: 110,
+		direction: undefined
+	});
+
+	useEffect(() => {
+		if (window.innerWidth <= 425)
+			setOptionsBelow({
+				size: 10,
+				direction: "vertical"
+			});
+		setScreenSize(window.innerWidth);
+	}, []);
+
+	let inputStyle = {
+		borderRadius: "5px"
+	};
+
+	if (screenSize <= 425) {
+		inputStyle.width = "80%";
+		inputStyle.padding = "3%";
+	} else {
+		inputStyle.width = "60%";
+		inputStyle.padding = "3%";
+	}
+
 	return (
 		<Form className="login_form" name="login_form">
 			<Form.Item
@@ -17,9 +44,12 @@ const LoginForm = ({ rememberMe }) => {
 				rules={[{ required: true, message: "Debe colocar su usuario" }]}
 			>
 				<Input
+					name="username"
 					placeholder="Username or email"
 					prefix={<UserOutlined />}
-					style={{ borderRadius: "5px", width: "60%", padding: "3%" }}
+					value={formData.username}
+					onChange={(e) => handleLoginForm(e)}
+					style={inputStyle}
 				/>
 			</Form.Item>
 			<Form.Item
@@ -29,25 +59,35 @@ const LoginForm = ({ rememberMe }) => {
 				<Input.Password
 					placeholder="Password"
 					prefix={<LockOutlined />}
+					value={formData.password}
 					iconRender={(visible) =>
 						visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
 					}
-					style={{ borderRadius: "5px", width: "60%", padding: "3%" }}
+					style={inputStyle}
 				/>
 			</Form.Item>
 
-			<Space size={110}>
+			<Space size={optionsBelow.size} direction={optionsBelow.direction}>
 				<Checkbox onChange={rememberMe}>Remember me</Checkbox>
-				<Button type="primary" size="large">
-					LOGIN
-				</Button>
+				{screenSize <= 425 ? (
+					<Button type="primary" size="large" block>
+						LOGIN
+					</Button>
+				) : (
+					<Button type="primary" size="large">
+						LOGIN
+					</Button>
+				)}
 			</Space>
 
-			<Space size={108}>
+			<br />
+			<br />
+
+			<Space size={optionsBelow.size} direction={optionsBelow.direction}>
 				<Button type="link" size="small">
 					Register now
 				</Button>
-				<Button type="link" size="small">
+				<Button type="link" size="small" style={{ color: "gray" }}>
 					Forgot password?
 				</Button>
 			</Space>
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -5,17 +5,40 @@ import "./css/App.css";
 import LoginForm from "../components/LoginForm";
 
 function App() {
+	const [form, setForm] = useState({
+		username: "",
+		password: ""
+	});
+
 	const rememberMe = () => console.log("Changed");
 
+	const handleLoginForm = (data) => {
+		setForm({
+			...form,
+			[data.target.name]: data.target.value
+		});
+	};
+
 	return (
-		<div className="card">
-			<div className="card__column">
-				<LoginForm rememberMe={rememberMe} />
-				<div className="card__separator">
-					or
+		<div className="prueba">
+			<div className="card">
+				<div className="card__column">
+					<LoginForm
+						rememberMe={rememberMe}
+						handleLoginForm={handleLoginForm}
+						formData={form}
+					/>
+					<div className="card__separator">or</div>
+				</div>
+				<div className="card__column card__column--right">
+					<img
+						src="https://i.pinimg.com/originals/1c/83/a2/1c83a220629444d73482a2f988e0abef.png"
+						alt="Login img"
+						height="600"
+						width="100%"
+					/>
 				</div>
 			</div>
-			<div className="card__column">Imagen</div>
 		</div>
 	);
 }
